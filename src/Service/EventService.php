7b6b911c1d4ace763fd035d6e70e57d8f180def3
Damien Hebert <damien-1998@live.fr>
2023-03-12 18:28:17 +0100

    fix: key metadata non défini

--- src/Service/EventService.php
+++ src/Service/EventService.php
@@ -19,34 +19,40 @@ class EventService extends ApiRequest
     {
         try {
             $event = json_decode($eventData, true, 512, JSON_THROW_ON_ERROR);
+            $eventType = $event['eventType'] ?? null;
+            $data = $event['data'] ?? null;
 
-            if (empty($event['eventType'])) {
+            if (!$eventType) {
                 throw new InvalidValueException('eventType is missing');
             }
 
-            if (empty($event['data'])) {
+            if (!$eventData) {
                 throw new InvalidValueException('data is missing');
             }
 
-            $data = json_encode($event['data'], JSON_THROW_ON_ERROR);
+            /** @var string $dataDecoded */
+            $dataDecoded = json_encode($data, JSON_THROW_ON_ERROR);
         } catch (\JsonException $e) {
             throw new InvalidValueException('data is malformed');
         }
 
-        if (Event::EVENT_TYPE_FORM === $event['eventType']) {
-            $data = $this->deserialize($data, FormPublicModel::class);
-        } elseif (Event::EVENT_TYPE_ORDER === $event['eventType']) {
-            $data = $this->deserialize($data, OrderDetail::class);
-        } elseif (Event::EVENT_TYPE_PAYMENT === $event['eventType']) {
-            $data = $this->deserialize($data, PaymentDetail::class);
+        if (Event::EVENT_TYPE_FORM === $eventType) {
+            /** @var FormPublicModel $dataObject */
+            $dataObject = $this->deserialize($dataDecoded, FormPublicModel::class);
+        } elseif (Event::EVENT_TYPE_ORDER === $eventType) {
+            /** @var OrderDetail $dataObject */
+            $dataObject = $this->deserialize($dataDecoded, OrderDetail::class);
+        } elseif (Event::EVENT_TYPE_PAYMENT === $eventType) {
+            /** @var PaymentDetail $dataObject */
+            $dataObject = $this->deserialize($dataDecoded, PaymentDetail::class);
         } else {
-            throw new InvalidValueException('eventType "'.$event['eventType'].'" not supported');
+            throw new InvalidValueException('eventType "'.$eventType.'" not supported');
         }
 
         return (new Event())
-            ->setMetadata($event['metadata'])
-            ->setData($data)
-            ->setEventType($event['eventType'])
+            ->setMetadata($event['metadata'] ?? [])
+            ->setData($dataObject)
+            ->setEventType($eventType)
         ;
     }
 }
